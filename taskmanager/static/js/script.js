f275d3cd99d3f3c11330db65463d92bbc60c0494
EwanColquhoun <ewancolquhoun@hotmail.com>
2021-09-23 15:01:41 +0000

    Add the edit task functionallity

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,14 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidednav initialisation
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+   
+document.addEventListener("DOMContentLoaded", function() {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
 
-    let datepicker= document.querySelectorAll('.datepicker');
-    M.Datepicker.init(datepicker, {
-        format: "dd mmmm, yyyy",
-        i18n: {done: "Select"},
-    });
-
-    let selects = document.querySelectorAll('select');
-    M.FormSelect.init(selects);
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
   });
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+
+  // collapsible initializataion
+  let collapsibles = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(collapsibles);
+});
